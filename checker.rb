861744384e9f5db446b8c43e85cde2e49be05aec
Gids <gidsgoldberg@gmail.com>
2018-10-23 23:19:39 +0100

    write result to file

--- checker.rb
+++ checker.rb
@@ -1,13 +1,18 @@
 require 'httparty'
 require 'register_client_manager'
 require 'json'
+require 'yaml'
 
 registers_client = RegistersClient::RegisterClientManager.new()
 register = registers_client.get_register('government-domain', 'beta')
-records = register.get_records.each{|r| 
+results = register.get_records.map{|r| 
    hostname =  "www.#{r.item.value['hostname']}.gov.uk"
    ssl_checker_url = "https://www.websecurity.symantec.com/bin/websitesecurity/sslcheckerinfo?h=#{hostname}"
    response = HTTParty.get(ssl_checker_url)
-   response_json = JSON.parse(response.body)
-   puts({hostname: hostname, response: response_json  })
+   result = { hostname: hostname, response: JSON.parse(response.body) }
+   puts(result)
+   result
 }
+File.open("results.yml", "w") { |file| file.write(YAML.dump(results)) }
+
+
