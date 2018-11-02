32a7dc9d11b4d271e1b4250582b827b8217485e6
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-11-02 16:32:40 +0000

    update to add is redirect check

--- process_results.rb
+++ process_results.rb
@@ -1,5 +1,12 @@
 require 'yaml'
 require 'spreadsheet_architect'
+require 'httparty'
+
+def is_redirect(hostname)
+  response = HTTParty.head('https://' + hostname, follow_redirects: false, verify: false)
+  response&.code == 301
+end
+
 results = YAML.load_file('results.yml')
 puts(results)
 with_symantec_certs = results.select{|r| 
@@ -7,8 +14,9 @@ with_symantec_certs = results.select{|r|
 }
 .sort_by{|r| r[:hostname]}
 
-headers = ['hostname', 'mainIssuer']
-data = with_symantec_certs.map{|r| [r[:hostname], r[:response]['mainIssuer']]}
+headers = ['hostname', 'main issuer', 'is redirect?']
+data = with_symantec_certs.map{|r|
+[r[:hostname], r[:response]['mainIssuer'], is_redirect(r[:hostname]) ]}
 file_data = SpreadsheetArchitect.to_csv(headers: headers, data: data)
 File.open('with_symantec_certs_2018_10_24.csv', 'w+b') do |f|
   f.write file_data
